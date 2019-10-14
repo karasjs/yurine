b4ee8c1531e5bbdbef7d903b42eafce44ddfa9a9
army8735 <army8735@qq.com>
2019-10-14 13:50:09 +0800

    refactor: 去除无用代码

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # yurine(百合音)
+
+[![NPM version](https://badge.fury.io/js/yurine.png)](https://npmjs.org/package/yurine)
+[![Build Status](https://travis-ci.org/karasjs/yurine.svg?branch=master)](https://travis-ci.org/karasjs/yurine)
+[![Dependency Status](https://david-dm.org/karasjs/yurine.png)](https://david-dm.org/karasjs/yurine)
+
--- src/csx.js
+++ src/csx.js
@@ -1,18 +1,15 @@
 import homunculus from 'homunculus';
-import InnerTree from './InnerTree';
-import linkage from './linkage';
+import Tree from './Tree';
 import ignore from './ignore';
 import join from './join';
-import join2 from './join2';
-import delegate from './delegate';
 
 let Token = homunculus.getClass('token', 'csx');
 let Node = homunculus.getClass('node', 'csx');
 
-function elem(node, opt, param) {
+function elem(node) {
   let res = '';
   //open和selfClose逻辑复用
-  res += selfClose(node.first(), opt, param);
+  res += selfClose(node.first());
   res += ',[';
   let comma = false;
   for(let i = 1, len = node.size(); i < len - 1; i++) {
@@ -23,7 +20,7 @@ function elem(node, opt, param) {
           res += ',';
           comma = false;
         }
-        res += child(leaf, opt, param);
+        res += child(leaf);
         comma = true;
         break;
       case Node.TOKEN:
@@ -51,7 +48,7 @@ function elem(node, opt, param) {
           res += ',';
           comma = false;
         }
-        res += parse(leaf, opt, param);
+        res += parse(leaf);
         comma = true;
     }
   }
@@ -61,7 +58,7 @@ function elem(node, opt, param) {
   }
   return res;
 }
-function selfClose(node, opt, param) {
+function selfClose(node) {
   let res = '';
   let name;
   let first = node.leaf(1);
@@ -94,10 +91,10 @@ function selfClose(node, opt, param) {
     }
     switch(leaf.name()) {
       case Node.CSXBindAttribute:
-        res += attr(leaf, opt, param);
+        res += attr(leaf);
         break;
       case Node.CSXAttribute:
-        res += attr(leaf, opt, param);
+        res += attr(leaf);
         break;
       case Node.CSXSpreadAttribute:
         res += spread(leaf);
@@ -107,18 +104,11 @@ function selfClose(node, opt, param) {
   res += ']';
   return res;
 }
-function attr(node, opt, param) {
+function attr(node) {
   let res = '';
   let key = node.first().token().content();
   let name = node.parent().leaf(1).token().content();
   let isCp = /^[A-Z]/.test(name);
-  if(key.charAt(0) === '@') {
-    key = key.slice(1);
-  }
-  // 组件属性非@申明均不bind
-  else if(isCp && opt.isBind) {
-    opt.isBind = false;
-  }
   let k = '["' + key + '"';
   res += k + ',';
   let v = node.last();
@@ -126,190 +116,27 @@ function attr(node, opt, param) {
     v = v.token().content();
     res += v;
   }
-  else if(/^on-?[a-zA-Z]/.test(key)) {
-    res += onEvent(v, opt, param);
-  }
   else {
-    res += child(v, opt, param, true);
+    res += child(v);
   }
   res += ']';
   return res;
 }
-function onEvent(node, opt, param) {
-  return delegate(node, param);
-}
 function spread(node) {
   return join(node.leaf(2));
 }
-function child(node, opt, param, isAttr) {
-  let callexpr = node.leaf(1);
-  if(opt.isBind) {
-    let temp = linkage(callexpr, param, {
-      arrowFn: opt.arrowFn,
-    });
-    let list = temp.arr;
-    let single = temp.single;
-    let bind = temp.bind;
-    if(list.length) {
-      let listener = list.length === 1
-        ? ('"' + list[0] + '"')
-        : JSON.stringify(list);
-      if(isAttr) {
-        let key = node.prev().prev().token().content();
-        if(key === 'value' || key === 'checked' || key === 'selected') {
-          let tag = node.parent().parent().leaf(1).token().content();
-          if(tag === 'input' || tag === 'select' || tag === 'option') {
-            let value = node.leaf(1);
-            // 单独值mmbexpr非运算符双向绑定，其余单向
-            if(value.name() === Node.MMBEXPR) {
-              let v = join2(value);
-              return 'new yurine.Obj('
-                + listener
-                + ',()=>{return('
-                + new InnerTree(opt, param).parse(node).replace(/^(\s*){/, '$1').replace(/}(\s*)$/, '$1')
-                + ')}'
-                + (single ? ',true' : ',false')
-                + (bind ?
-                  (',(v)=>{v!=='
-                  + v
-                  + '&&('
-                  + v
-                  + '=v)})')
-                  : ')');
-            }
-            return 'new yurine.Obj('
-              + listener
-              + ',()=>{return('
-              + new InnerTree(opt, param).parse(node).replace(/^(\s*){/, '$1').replace(/}(\s*)$/, '$1')
-              + ')}'
-              + (single ? ',true' : '')
-              + ')';
-          }
-        }
-      }
-      else if(node.prev() && node.prev().name() === Node.CSXOpeningElement) {
-        let key = node.prev().leaf(1).token().content();
-        if(key === 'textarea') {
-          let value = node.leaf(1);
-          if(value.name() === Node.MMBEXPR) {
-            let v = join2(value);
-            return 'new yurine.Obj('
-              + listener
-              + ',()=>{return('
-              + new InnerTree(opt, param).parse(node).replace(/^(\s*){/, '$1').replace(/}(\s*)$/, '$1')
-              + ')}'
-              + (single ? ',true' : ',false')
-              + (bind ?
-                (',(v)=>{v!=='
-                  + v
-                  + '&&('
-                  + v
-                  + '=v)})')
-                : ')');
-          }
-        }
-      }
-      return 'new yurine.Obj('
-        + listener
-        + ',()=>{return('
-        + new InnerTree(opt, param).parse(node).replace(/^(\s*){/, '$1').replace(/}(\s*)$/, '$1')
-        + ')}'
-        + (single ? ',true' : '')
-        + ')';
-    }
-  }
-  // Obj中再次出现的:input的value还需要添加Obj
-  else if(opt.isInBind) {
-    if(isAttr) {
-      let key = node.prev().prev().token().content();
-      if(key === 'value') {
-        let tag = node.parent().parent().leaf(1).token().content();
-        if(tag === 'input' || tag === 'select') {
-          let temp = linkage(callexpr, param, {
-            arrowFn: opt.arrowFn,
-          });
-          let list = temp.arr;
-          let bind = temp.bind;
-          if(list.length) {
-            let value = node.leaf(1);
-            let listener = list.length === 1
-              ? ('"' + list[0] + '"')
-              : JSON.stringify(list);
-            if(value.name() === Node.MMBEXPR) {
-              let v = join2(value);
-              return 'new yurine.Obj('
-                + listener
-                + ',()=>{return('
-                + new InnerTree(opt, param).parse(node).replace(/^(\s*){/, '$1').replace(/}(\s*)$/, '$1')
-                + ')}'
-                + ',false'
-                + (bind ?
-                  (',(v)=>{v!=='
-                    + v
-                    + '&&('
-                    + v
-                    + '=v)})')
-                  : ')');
-            }
-            return 'new yurine.Obj('
-              + listener
-              + ',()=>{return('
-              + new InnerTree(opt, param).parse(node).replace(/^(\s*){/, '$1').replace(/}(\s*)$/, '$1')
-              + ')})';
-
-          }
-        }
-      }
-    }
-    else if(node.prev() && node.prev().name() === Node.CSXOpeningElement) {
-      let key = node.prev().leaf(1).token().content();
-      if(key === 'textarea') {
-        let temp = linkage(callexpr, param, {
-          arrowFn: opt.arrowFn,
-        });
-        let list = temp.arr;
-        let bind = temp.bind;
-        if(list.length) {
-          let value = node.leaf(1);
-          let listener = list.length === 1
-            ? ('"' + list[0] + '"')
-            : JSON.stringify(list);
-          if(value.name() === Node.MMBEXPR) {
-            let v = join2(value);
-            return 'new yurine.Obj('
-              + listener
-              + ',()=>{return('
-              + new InnerTree(opt, param).parse(node).replace(/^(\s*){/, '$1').replace(/}(\s*)$/, '$1')
-              + ')}'
-              + ',false'
-              + (bind ?
-                (',(v)=>{v!=='
-                  + v
-                  + '&&('
-                  + v
-                  + '=v)})')
-                : ')');
-          }
-          return 'new yurine.Obj('
-            + listener
-            + ',()=>{return('
-            + new InnerTree(opt, param).parse(node).replace(/^(\s*){/, '$1').replace(/}(\s*)$/, '$1')
-            + ')})';
-        }
-      }
-    }
-  }
-  return new InnerTree(opt, param).parse(node).replace(/^(\s*){/, '$1').replace(/}(\s*)$/, '$1');
+function child(node) {
+  return new Tree().parse(node).replace(/^(\s*){/, '$1').replace(/}(\s*)$/, '$1');
 }
 
-function parse(node, opt, param) {
+function parse(node) {
   let res = '';
   switch(node.name()) {
     case Node.CSXElement:
-      res += elem(node, opt, param);
+      res += elem(node);
       break;
     case Node.CSXSelfClosingElement:
-      res += selfClose(node, opt, param);
+      res += selfClose(node);
       res += ')';
       break;
   }
