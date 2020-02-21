45b5a86df24e488ca4eb1cbb908f386a149be743
army8735 <army8735@qq.com>
2020-02-21 17:40:45 +0800

    chore: 压缩配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
 
-export default {
+export default [{
   input: 'src/index.js',
   output: {
     name: 'yurine',
@@ -17,4 +18,24 @@ export default {
       runtimeHelpers: true
     }),
   ],
-};
+}, {
+  input: 'src/index.js',
+  output: {
+    name: 'yurine',
+    file: 'index.min.js',
+    format: 'umd',
+    sourcemap: true,
+    globals: {
+      homunculus: 'homunculus',
+    },
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**', // 只编译我们的源代码
+      runtimeHelpers: true
+    }),
+    uglify({
+      sourcemap: true,
+    }),
+  ],
+}];
